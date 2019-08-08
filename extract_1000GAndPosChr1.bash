757b97d31a2d4edd99ef280a8634048f906421fa
jeantristan <jeantristanb@free.fr>
2019-08-08 16:14:27 +0200

    build data example

---
+++ extract_1000GAndPosChr1.bash
@@ -0,0 +1,25 @@
+## take of position postion dna chip
+DirData=data/
+mkdir -p $DirData/
+Chro=1
+FileBimH3africa=""
+## dpwload 1000G data
+wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/"ALL.chr"$Chro".phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz"
+listposchip=$DirData/"lispos."$Chro".bed"
+## extract positon of h3africa from a bim file  : bed output
+if [ ! -f $listposchip ]
+then
+awk -v chro=$Chro '{if($1==chro)print $1"\t"$4"\t"$4"\t"$2}' $FileBimH3africa > $listposchip
+fi
+
+## build  plink file with vcf and position
+if [ ! -f $DirData"/1000G.h3aPos."$Chro".vcf.bed" ]
+then
+plink --vcf ALL.chr"$Chro".phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz --extract range $listposchip --make-bed -out $DirData"/1000G.h3aPos."$Chro".vcf"
+fi
+
+## simulate phenotype with h3agwas pipleine
+nextflow -c simul.config run h3agwas/assoc/simul-assoc.nf -r hackaton --input_dir $DirData --input_pat "1000G.h3aPos."$Chro".vcf"  --output_dir "Chr"$Chro"Test"/  --out "pheno."$Chro --profile slurm -resume
+# delete  1000G file
+rm -r "ALL.chr"$Chro".phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz"
+
